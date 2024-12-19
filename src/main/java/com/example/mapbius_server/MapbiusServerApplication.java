7444963a4c79ab356bbef52881e30d9b2b146430
YGH <ba6853ak@naver.com>
2024-12-19 10:55:51 +0900

    Initial commit

---
+++ src/main/java/com/example/mapbius_server/MapbiusServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapbius_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapbiusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapbiusServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mapbius_server/MapbiusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapbius_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapbiusServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
