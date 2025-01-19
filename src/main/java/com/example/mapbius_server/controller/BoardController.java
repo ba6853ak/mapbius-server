6e64b60f0f64a47f11ea9c37fd8ac9b24b620b5a
YGH <ba6853ak@naver.com>
2025-01-19 21:52:55 +0900

    Feat: 좋아요 기능 수정2

--- src/main/java/com/example/mapbius_server/controller/BoardController.java
+++ src/main/java/com/example/mapbius_server/controller/BoardController.java
@@ -61,8 +61,6 @@ public class BoardController {
     @PostMapping("/api/private/reviews/heart")
     public ResponseEntity<?> reviewHeart(@RequestHeader("Authorization") String authorizationHeader, @RequestBody Review review) {
 
-        int state = 0;
-
         String token = authorizationHeader.replace("Bearer ", ""); // 토큰 추출
         String creator_id = jwtUtil.validateToken(token).getSubject(); // 토큰 검증
         review.setUserId(creator_id);
@@ -75,18 +73,24 @@ public class BoardController {
             return ResponseEntity.status(200).body(responseData);
         }
 
+        int state = boardService.reviewHeartAdd(creator_id, review.getReviewId());
 
-
-        if (boardService.reviewHeartAdd(creator_id, review.getReviewId()) == 1) {
+        if (state == 1) {
             responseData.setCode(201);
             responseData.setMessage("해당 게시글에 대한 좋아요를 해제했습니다.");
             return ResponseEntity.status(201).body(responseData);
         }
-        if (boardService.reviewHeartAdd(creator_id, review.getReviewId()) == 3) {
-            responseData.setCode(201);
+        if (state == 3) {
+            responseData.setCode(202);
             responseData.setMessage("해당 게시글에 대해 좋아요를 눌렀습니다.");
-            return ResponseEntity.status(201).body(responseData);
+            return ResponseEntity.status(202).body(responseData);
         }
+/*        else if (state == 4 || state == 2) {
+            responseData.setCode(404);
+            responseData.setMessage("변경 실패! 매개변수를 확인하세요.");
+            System.out.println("좋아요 상태 변경 실패");
+            return ResponseEntity.status(404).body(responseData);
+        }*/
         else {
             responseData.setCode(404);
             responseData.setMessage("변경 실패! 매개변수를 확인하세요.");
